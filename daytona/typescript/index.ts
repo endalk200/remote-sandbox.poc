dfa844872cebef6e1741dd26a7613ca65b79c405
endalk200 <eb808826@gmail.com>
2025-10-26 21:04:55 +0300

    feat: add Python and TypeScript sandbox implementations with of daytona experimentation

---
+++ daytona/typescript/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { Daytona } from "@daytonaio/sdk";
+
+// Initialize the Daytona client
+const daytona = new Daytona({ apiKey: process.env.DAYTONA_API_KEY });
+
+// Create the Sandbox instance
+const sandbox = await daytona.create({
+  language: "typescript",
+});
+
+// Run the code securely inside the Sandbox
+const response = await sandbox.process.codeRun(
+  'console.log("Hello World from code!")',
+);
+console.log(response.result);
+
+// Clean up
+await sandbox.delete();
